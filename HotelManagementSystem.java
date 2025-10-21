c21d153c09ad65fce2c30fc268e15e0d1bceecb5
Aliza Syed <alizas9k@gmail.com>
2025-10-21 16:50:29 +0000

    .

--- HotelManagementSystem.java
+++ HotelManagementSystem.java
@@ -1,7 +1,11 @@
-package src.hotelmanagementsystem;
+import javax.swing.*;
 
-public class HotelManagementSystem {
+public class HotelManagementSystem extends JFrame {
+    HotelManagementSystem() {
+        setSize(1366,565);//exact size of the image
+        setVisible(true);
+    }
     public static void main(String[] args) {
-       
+       new HotelManagementSystem();
     }
 }
